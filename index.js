e1e179393ba9ad6c7c2b7e325caf2425e5567c26
skycode <sikiruyaya1@gmail.com>
2025-02-17 22:22:09 +0100

    unit converter updated file

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const numEl = document.getElementById("num-el")
 const convertBtn = document.getElementById("convert-btn")
 
 convertBtn.addEventListener('click', function() {
-    lenMessageEl.textContent = `${numEl.value} metre = ${metreToFeet(numEl.value)} feet`
-    volumeMessageEl.textContent = `${numEl.value} litre = ${litreToGallon(numEl.value)} gallon`
-    massMessageEl.textContent = `${numEl.value} kilogram = ${kilogramToPound(numEl.value)} pound`
+    lenMessageEl.textContent = `${numEl.value} metre = ${metreToFeet(numEl.value)} feet | ${numEl.value} feet = ${feetToMetre(numEl.value)} metre`
+    volumeMessageEl.textContent = `${numEl.value} litre = ${litreToGallon(numEl.value)} gallon | {numEl.value} gallon = ${gallonsToLitres(numEl.value)} litre`
+    massMessageEl.textContent = `${numEl.value} kilogram = ${kilogramToPound(numEl.value)} pound | ${numEl.value} pound = ${poundToKilogram(numEl.value)} kilogram`
 })
 
 const oneMetreToFeet = 3.28084
 const oneLitreToGallon =  0.264172
 const oneKilogramToPound = 2.2046
+const oneFeetToMetre = 0.3048
+const oneGallonToLitre = 3.78541
+const onePoundToKilogram = 0.45359237
 
 function metreToFeet(num) {
     return (num * oneMetreToFeet).toFixed(3)
@@ -24,4 +27,16 @@ function litreToGallon(num) {
 
 function kilogramToPound(num) {
     return (num * oneKilogramToPound).toFixed(3)
+}
+
+function feetToMetre(num) {
+    return (num * oneFeetToMetre).toFixed(3)
+}
+
+function gallonsToLitres(num) {
+    return (num * oneGallonToLitre).toFixed(3)
+}
+
+function poundToKilogram(num) {
+    return (num * onePoundToKilogram).toFixed(3)
 }
